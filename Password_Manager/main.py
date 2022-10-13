aaf98663915419bbb169db9820081eac3014333c
gaw3568 <gaw3568@gmail.com>
2022-10-13 23:32:54 +0900

    Search 기능 추가

--- Password_Manager/main.py
+++ Password_Manager/main.py
@@ -22,13 +22,11 @@ def pswd_generate():
         password_entry.insert(0,final_pswd)
     else:
         password_entry.insert(0,final_pswd)
-    
-
 
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save():
-    website = website_entry.get()
+    website = website_entry.get().title()
     email = email_entry.get()
     password = password_entry.get()
     new_data = {
@@ -39,14 +37,14 @@ def save():
     }
 
     if len(website) == 0 or len(password) == 0:
-        messagebox.showinfo(title="Oops", message="There is an Empty Field.")
+        messagebox.showinfo(title="Oops", message="There is an Empty Field.", icon='question')
     else:
         try:
             with open("data.json", 'r') as data_file:
                 # Reading old data
                 data = json.load(data_file)
         except FileNotFoundError:
-            with open("data.json", "w")as data_file:
+            with open("data.json", "w") as data_file:
                 json.dump(new_data, data_file, indent=4)
         else:
             data.update(new_data)
@@ -59,6 +57,21 @@ def save():
             password_entry.delete(0,END)
             messagebox.showinfo(title="Password Create", message="It is created.")
 
+# ---------------------------- FIND PASSWORD ------------------------------- #
+def search_password():
+    website = website_entry.get().title()
+    try:
+        with open("data.json", "r") as data_file:
+            data = json.load(data_file)
+    except FileNotFoundError:
+        messagebox.showinfo(title="Error", message="No data file found", icon="error")
+    else:
+        if website in data:
+            email_info = data[website]["email"]
+            password_info = "*" * len(data[website]["password"])
+            messagebox.showinfo(title="Search", message=f"E-mail : {email_info}\n Password : {password_info}", icon="info")
+        else:
+            messagebox.showinfo(title="Search", message=f"There is no {website} named", icon='question')
 
 
 
@@ -82,16 +95,18 @@ password_label = Label(text="Password:")
 password_label.grid(row=3, column=0)
 
 # Entries
-website_entry = Entry(width=35)
-website_entry.grid(row=1, column=1, columnspan=2)
+website_entry = Entry(width=21)
+website_entry.grid(row=1, column=1)
 website_entry.focus()
-email_entry = Entry(width=35)
+email_entry = Entry(width=38)
 email_entry.grid(row=2, column=1, columnspan=2)
 email_entry.insert(0,"abcd@gmail.com")
-password_entry = Entry(width=19)
+password_entry = Entry(width=21)
 password_entry.grid(row=3, column=1)
 
 # Buttons
+search_button = Button(text="Search", width=13, command=search_password)
+search_button.grid(row=1, column=2)
 generate_pswd_button = Button(text="Generate Password", command=pswd_generate)
 generate_pswd_button.grid(row=3, column=2)
 add_button = Button(text="Add", width=35, command=save)
