e3ac247adb75f05199058f3eee942429663ebc3e
gaw3568 <gaw3568@gmail.com>
2022-10-13 10:36:19 +0900

    KeyError 예외 추가

--- Nato_Alphabet/main.py
+++ Nato_Alphabet/main.py
@@ -1,9 +1,18 @@
 import pandas as pd
 
 df = pd.read_csv("nato_phonetic_alphabet.csv")
+
 nato_dict = {value.letter:value.code for (key, value) in df.iterrows()}
+print(nato_dict)
 
-user_input = input("Enter a number : ").upper()
-
-nato_list = [nato_dict[letter] for letter in user_input]
-print(nato_list)
+# 발생하는 Error를 exception으로 catch
+is_not_error = False
+while not is_not_error:
+    try:
+        user_input = input("Enter a number : ").upper()
+        nato_list = [nato_dict[letter] for letter in user_input]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+    else:
+        print(nato_list)
+        is_not_error = True
