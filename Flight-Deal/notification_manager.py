98af75841624a11de48516c2dd470cc8a1360ba5
gaw3568 <40763988+gaw3568@users.noreply.github.com>
2022-11-11 01:54:10 +0900

    Update 예정
    Update 예정

--- Flight-Deal/notification_manager.py
+++ Flight-Deal/notification_manager.py
@@ -1,3 +1,6 @@
+# Twilio API를 사용하여 구글시트에 저장된 금액보다 저렴하면 SMS로 전송
+# 코드 수정 예정
+
 class NotificationManager:
     #This class is responsible for sending notifications with the deal flight details.
     pass
