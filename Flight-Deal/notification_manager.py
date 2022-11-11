6178a6919c5fd9bb4db817c08b76c3eac4e08056
gaw3568 <gaw3568@gmail.com>
2022-11-11 16:37:39 +0900

    add

--- Flight-Deal/notification_manager.py
+++ Flight-Deal/notification_manager.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from smtplib import SMTP
 
 class NotificationManager:
@@ -13,11 +12,3 @@ class NotificationManager:
                     to_addr = each_email,
                     msg=f"Subject:New Low Price Flight!\n\n{message}\n{google_flight_link}".encode('utf-8')
                 )
-=======
-# Twilio API를 사용하여 구글시트에 저장된 금액보다 저렴하면 SMS로 전송
-# 코드 수정 예정
-
-class NotificationManager:
-    #This class is responsible for sending notifications with the deal flight details.
-    pass
->>>>>>> refs/remotes/origin/main
